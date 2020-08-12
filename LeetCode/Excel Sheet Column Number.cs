422ca17fa74809b8845f57c833b209a7243e64d3
Biplab Sah <biplabsah@live.com>
2020-08-13 02:07:42 +0530

    Excel Sheet Column Number

---
+++ LeetCode/Excel Sheet Column Number.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Excel_Sheet_Column_Number
+    {
+
+        public int TitleToNumber(string s)
+        {
+
+            int res = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                res = res * 26 + (s[i] - 'A' + 1);
+
+            }
+            return res;
+        }
+
+    }
+}
+
+/*
+ * 
+ * Date: 11 Aug 2020
+ * 
+ Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+For example: 
+
+A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28 
+
+Constraints:
+
+1 <= s.length <= 7
+s consists only of uppercase English letters.
+s is between "A" and "FXSHRXW".
+
+*/
