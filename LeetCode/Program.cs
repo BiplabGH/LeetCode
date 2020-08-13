9578da9d5eaa70f52b9085e2b2172a96a6d5b241
Biplab Sah <biplabsah@live.com>
2020-08-13 17:09:18 +0530

    LC2 Pascal number, works for less than 30.

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LeetCode
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region 1 - Excel_Sheet_Column_Number
+            /*
+            Excel_Sheet_Column_Number escn = new Excel_Sheet_Column_Number();
+            int colNum = escn.TitleToNumber("A");
+            Console.ReadLine(colNum);
+            */
+            #endregion
+
+            #region 2 - Pascal_Number
+            /*
+            Pascal_Number pn = new Pascal_Number();
+            IList<int> myList = pn.GetRow(30);
+            Console.WriteLine(myList);
+            */
+            #endregion
         }
     }
 }
