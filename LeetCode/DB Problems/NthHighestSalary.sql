6a433e082429f2478690aa6afc04778b1f1d34f6
Biplab Sah <biplabsah@live.com>
2020-08-18 01:01:15 +0530

    DB3-Nth Highest Salary

---
+++ LeetCode/DB Problems/NthHighestSalary.sql
@@ -0,0 +1,29 @@
+﻿/*
+Write a SQL query to get the nth highest salary from the Employee table.
+
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+For example, given the above Employee table, the nth highest salary where n = 2 is 200. If there is no nth highest salary, then the query should return null.
+
++------------------------+
+| getNthHighestSalary(2) |
++------------------------+
+| 200                    |
++------------------------+
+*/
+
+CREATE FUNCTION getNthHighestSalary(@N INT) RETURNS INT AS
+BEGIN
+    RETURN (
+        /* Write your T-SQL query statement below. */
+
+        select MAX(Salary) from Employee
+        where Salary not in 
+        (select distinct Top (@N - 1) Salary from Employee order by Salary desc)
+    );
+END
